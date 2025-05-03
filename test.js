2ec74c932c267d591f178023f743c378c5699e24
amir830047agh@gmail.com <amir830047agh@gmail.com>
2025-05-03 22:24:05 +0330

    js nesting func part2

--- test.js
+++ test.js
@@ -27,3 +27,15 @@ function sum(a, b, c) {
   return a + b + c;
 }
 console.log(sum(2, 3, 5));
+
+function curry(fn) {
+  return function (a) {
+    return function (b) {
+      return function (c) {
+        return fn(a, b, c);
+      };
+    };
+  };
+}
+const currisum = curry(sum);
+console.log(currisum(2(3)(5)));
