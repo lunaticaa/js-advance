c6fa392f71024f2b0a0cedc6034a26b78f4a415a
amir830047agh@gmail.com <amir830047agh@gmail.com>
2025-05-05 20:08:47 +0330

    js using iterator and of in for

--- test.js
+++ test.js
@@ -100,6 +100,7 @@
 // console.log(Person2.getFullName());
 // console.log(Person1.getFullName());
 
+// class extends
 class person {
   constructor(fname, lName) {
     this.firstName = fname;
@@ -126,3 +127,26 @@ class superHero extends person {
 
 const Batman = new superHero("amir", "wayne");
 console.log(Batman.sayMyName());
+
+// Iterables and Iterators
+const obj = {
+  [Symbol.iterator]: function () {
+    let step = 0;
+    const iterator = {
+      next: function () {
+        step++;
+        if (step === 1) {
+          return { value: "Hello", done: false };
+        } else if (step === 2) {
+          return { value: "World", done: false };
+        }
+        return { value: undefined, done: true };
+      },
+    };
+    return iterator;
+  },
+};
+
+for (const word of obj) {
+  console.log(word);
+}
